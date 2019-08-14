b0a2e0646cca111df679e626fd0702529cbfe379
HiroIshida <spitfire.docomo@gmail.com>
2019-08-14 11:46:44 +0900

    make player class

--- client.py
+++ client.py
@@ -4,29 +4,35 @@
 import socket
 import json
 
-def gen_message(method, args):
-    data = dict()
-    data["method"] = method
-    data["args"] = args
-    str_data = json.dumps(data)
-    bin_data = str_data.encode()
-    return bin_data
+class Player:
+    def __init__(self, host, port):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((host, port))
 
-def get_put_message(pos):
-    args = {'position': pos}
-    return gen_message("put", args)
+    def put(self, position):
+        method = "put"
+        args = {'position': position}
+        self._send(method, args)
 
-def get_show_message():
-    args = {}
-    return gen_message("show", args)
+    def show(self):
+        method = "show"
+        args = {}
+        self._send(method, args)
+
+    def _send(self, method, args):
+        data = dict()
+        data["method"] = method
+        data["args"] = args
+        str_data = json.dumps(data)
+        message_send = str_data.encode()
+        self.client.sendall(message_send)
+        message_recieved = self.client.recv(1024)
+        print(message_recieved)
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
 
-message = get_put_message((1, 1))
-client.sendall(message)
-data_recv = client.recv(1024)
-print('Received', repr(data_recv))
+player = Player(HOST, PORT)
+player.put((1, 1))
+player.show()
 
