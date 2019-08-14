993849a5394ecd435e64d79d0e5eef893d22aa4e
HiroIshida <spitfire.docomo@gmail.com>
2019-08-14 12:41:54 +0900

    loop handler

--- server.py
+++ server.py
@@ -3,6 +3,8 @@
 import socket
 import json
 import base64
+import threading
+import select
 from tictoctoe import TictactoeGame
 
 game_field = TictactoeGame(3)
@@ -14,32 +16,33 @@ def dispatch(method, args):
     if method == "show":
         game_field.show()
 
-"""
-data = dict()
-data["method_name"] = "put"
-data["args"] = {"position": (1, 2)}
-dispatch(data["method_name"], data["args"])
-"""
 
 HOST = '127.0.0.1'
 PORT = 65432
+
+
+def loop_handler(connection, address):
+    while True:
+        bin_data = connection.recv(1024)
+        if not bin_data:
+            break
+        str_data = bin_data.decode()
+        dict_data = json.loads(str_data)
+
+        method = dict_data["method"]
+        args = dict_data["args"]
+        dispatch(method, args)
+        conn.sendall(b'received')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     while True:
         conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                bin_data = conn.recv(1024)
-                if not bin_data:
-                    break
-                str_data = bin_data.decode()
-                dict_data = json.loads(str_data)
+        loop_handler(conn, addr)
 
-                method = dict_data["method"]
-                args = dict_data["args"]
-                dispatch(method, args)
-                conn.sendall(b'fuck')
+
+
+
+
 
