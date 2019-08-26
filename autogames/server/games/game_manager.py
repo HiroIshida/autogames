8c53e269670199cc7723516030dcfceb8afae73e
Naoya Yamaguchi <708yamaguchi@gmail.com>
2019-08-26 16:11:21 +0900

    move put() function from tictactoe_game.py to game_manager.py

--- autogames/server/games/game_manager.py
+++ autogames/server/games/game_manager.py
@@ -1,10 +1,42 @@
 class GameManager:
 
-    def __init__(self, N_player):
+    def __init__(self, N_player, dim):
         self.N_player = N_player  # how many player can join this game
         self.player_numbers = range(1, N_player + 1)  # [1, 2, ... N_player]
         self.current_players = 0  # the number of current online players
         self.counter = 0
+        self.dim = dim  # size of game board (dim * dim size board)
+        self.field = [[0 for x in range(dim)] for y in range(dim)]
+        self.isGameFinish = False
+
+    # piece means 'koma' or 'stone'
+    def _put(self, player_number, position, piece):
+        # return from this method if invalid operation is executed
+        x = position[0]
+        y = position[1]
+        if position not in self.available_positions():
+            if x < 0 or x >= self.dim or y < 0 or y >= self.dim:
+                print("out of the game field")
+            else:
+                print("there is already a stone here")
+            return(True, 'Invalid operation !')
+
+        current_turn_player = self.whos_turn()
+
+        if not player_number == current_turn_player:
+            return (True, "please wait your opponent for finishing the turn")
+
+        # put a stone;
+        self.field[x][y] = piece
+
+        # check checkmate
+        result = self._check_checkmate(player_number)
+        isGameEnd = result[0]
+        message = result[1]
+
+        if not isGameEnd:
+            self.go_next_turn()
+        return (not isGameEnd, message)
 
     def add_player(self):
         print("new player is set")
