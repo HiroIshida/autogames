ff1a7399c9c58a793a38522a3d5efb5bc6a8fe1c
Naoya Yamaguchi <708yamaguchi@gmail.com>
2019-08-27 15:21:21 +0900

    add user input mode in which human can fight against agents

---
+++ autogames/test/check_human_mode.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#########################################################################################
+# This script cannot locate in .travis.yml because travis system does not have keyboard #
+#########################################################################################
+
+COUNT=0
+SCRIPTS=("autogames_server --game tictactoe_game --port 65432 &" # server
+         "python autogames/client/python/client.py --port 65432 --agent-file human_agent --timeout 30 &" # human mode
+         "python autogames/client/python/client.py --port 65432 --agent-file example_agent_tictactoe --timeout 30 &" # client2 with Python
+        )
+
+for ((i = 0; i < ${#SCRIPTS[@]}; i++))
+do
+    eval "${SCRIPTS[$i]}"
+    pids[$COUNT]=$!
+    COUNT=$((COUNT+1))
+    sleep 0.3
+done
+
+
+# exit 0 only when all processes (server and client) finished with 0
+# See https://stackoverflow.com/questions/356100/how-to-wait-in-bash-for-several-subprocesses-to-finish-and-return-exit-code-0
+for pid in ${pids[*]}; do
+    wait $pid
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+done
