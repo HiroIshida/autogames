6d71e6343349ac72d6f4005a9c431c006b74861b
Naoya Yamaguchi <708yamaguchi@gmail.com>
2019-08-27 16:54:08 +0900

    introduce Makefile for C compile

--- autogames/test/c/check_c_clients.sh
+++ autogames/test/c/check_c_clients.sh
@@ -2,7 +2,7 @@
 
 COUNT=0
 SCRIPTS=("autogames_server --game tictactoe_game --port 65432 &" # server
-         "cd autogames/client/c/; gcc -Wall -g -O2 -o client-c agent.c client.c client_init.c json_utils.c -L /usr/lib/i386-linux-gnu -ljson-c; cd -; ./autogames/client/c/client-c 65432 &" # client with C
+         "cd autogames/client/c/; make client_c AGENT_FILE=example_agent_tictactoe.c; cd -; ./autogames/client/c/client_c 65432 &" # client with C
          "python autogames/client/python/client.py --port 65432 --agent-file example_agent_tictactoe &" # client with Python
         )
 
@@ -14,6 +14,7 @@ do
     sleep 0.3
 done
 
+rm ./autogames/client/c/client_c
 
 # exit 0 only when all processes (server and client) finished with 0
 # See https://stackoverflow.com/questions/356100/how-to-wait-in-bash-for-several-subprocesses-to-finish-and-return-exit-code-0
